dc12cdbf7170603a50e8bf0f6f727f9e24974bb5
User <abenkov82@gmail.com>
2022-03-18 13:13:27 +0400

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Alex Benkov");
+        System.out.println("06.04.1982");
+    }
+}
